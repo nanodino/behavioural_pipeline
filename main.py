e3b4ca0bcf33282e705e93a9c7e083b62b86d639
nancy <cantu.nancy@gmail.com>
2023-09-04 11:01:21 -0400

    start organizing script into functions + add type annotations

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from pathlib import Path
 from glob import glob
 import csv
 
-data_path = Path("./data/")
-data_files = glob('*.tsv', root_dir=data_path, recursive=False)
-dfs_dict = {}
-columns_of_interest = ['Observation id',
-                       'Observation date', 'Subject', 'Behavior',
-                       'Modifier #1', 'Modifier #2', 'Time',
-                       'Behavior type', 'Start (s)', 'Stop (s)', 'Duration (s)']
+def get_input_data_files() -> dict:
+    data_path = Path("./data/")
+    data_files = glob('*.tsv', root_dir=data_path, recursive=False)
+    dfs_dict = {}
+    columns_of_interest = ['Observation id',
+                           'Observation date', 'Subject', 'Behavior',
+                           'Modifier #1', 'Modifier #2', 'Time',
+                           'Behavior type', 'Start (s)', 'Stop (s)', 'Duration (s)']
 
+    for file in data_files:
+        print(f'Reading columns for file {file}')
+        with open(f'{data_path}/{file}') as f:
+            dfs_dict[file] = pd.read_csv(f, delimiter='\t')
+            dfs_dict[file].drop(columns=[col for col in dfs_dict[file]
+                                         if col not in columns_of_interest], inplace=True)
 
-for file in data_files:
-    print(f'Reading columns for file {file}')
-    with open(f'{data_path}/{file}') as f:
-        dfs_dict[file] = pd.read_csv(f, delimiter='\t')
-        dfs_dict[file].drop(columns=[col for col in dfs_dict[file]
-                                     if col not in columns_of_interest], inplace=True)
-        print(dfs_dict[file])
+    return dfs_dict
