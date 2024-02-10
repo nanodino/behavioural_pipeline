f4106ae46d54684b091d37ebcf4e1b9e3a55e3aa
Nancy Cantu <cantu.nancy@gmail.com>
2024-02-10 11:17:04 -0500

    add basic new app with different dfs for each subject
    works on streamlit, displays data for debugging and for POC

---
+++ new_app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+
+BR_AREAS = ['A', 'B', 'C', 'D']
+RT_AREAS = ['H', 'I', 'J']
+TWRL_AREAS = ['A', 'D']
+BKFL_AREAS = ['E', 'F']
+LIRDRT_AREAS = ['A', 'D']
+
+
+def import_input_files() ->  dict[str, pd.DataFrame]:
+    input_data_tables_dict= {}
+    columns_of_interest = ['Observation id',
+                           'Subject', 'Behavior',
+                           'Behavior type', 'Time', 'Observation date', 'Observation duration']
+    data_files = st.file_uploader("Upload your data files", type=['csv', 'tsv'], accept_multiple_files=True)
+    if data_files is not None:
+        for file in data_files:
+            file.seek(0)
+            print(f'Reading columns for file {file.name}')
+            input_data_tables_dict[file.name] = pd.read_csv(
+                file, delimiter='\t')
+            input_data_tables_dict[file.name].drop(columns=[col for col in input_data_tables_dict[file.name]
+                                                            if col not in columns_of_interest], inplace=True)
+
+    return input_data_tables_dict
+
+def concatenate_data_from_all_observations(input_data_tables_dict) -> pd.DataFrame:
+    print('Concatenating data from all observations')
+    data = pd.concat(input_data_tables_dict.values(), ignore_index=True)
+    return data
+
+def separate_data_by_subject(data: pd.DataFrame) -> dict[str, pd.DataFrame]:
+    print('Separating data by subject')
+    data_by_subject = {}
+    for subject in data['Subject'].unique():
+        data_by_subject[subject] = data[data['Subject'] == subject]
+    return data_by_subject
+
+def run_pipeline(dfs: dict[str, pd.DataFrame]):
+    data = concatenate_data_from_all_observations(dfs)
+    data_by_subject = separate_data_by_subject(data)
+    st.write(data_by_subject)
+    st.dataframe(data_by_subject['DMO-10'])
+
+def main():
+    st.title("Behavioural analysis pipeline")
+    st.header("Welcome to the behavioural analysis pipeline!")
+    st.write("This pipeline takes in .tsv files and outputs an excel file with behavioural analysis data and statistics.")
+    st.write("Please upload your data files below.")
+    dfs = import_input_files()
+    # only run if there are files
+    if dfs:
+        run_pipeline(dfs)
+
+
+if __name__ == "__main__":
+    main()
