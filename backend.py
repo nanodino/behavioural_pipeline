4da8d1fa98d611e895076f4cc4dbec572a322640
Nancy Cantu <cantu.nancy@gmail.com>
2024-02-13 22:25:53 -0500

    add new backend file

---
+++ backend.py
@@ -0,0 +1,113 @@
+import pandas as pd
+from intervaltree import Interval, IntervalTree
+import numpy as np
+
+BR_AREAS = ['A', 'B', 'C', 'D']
+RT_AREAS = ['H', 'I', 'J']
+TWRL_AREAS = ['A', 'D']
+BKFL_AREAS = ['E', 'F']
+LIRDRT_AREAS = ['A', 'D']
+
+
+def separate_data_by_subject(data: pd.DataFrame) -> dict[str, pd.DataFrame]:
+    print('Separating data by subject')
+    data_by_subject = {}
+    for subject in data['Subject'].unique():
+        data_by_subject[subject] = data[data['Subject'] == subject]
+    return data_by_subject
+
+
+def get_behaviour_modifiers(df: pd.DataFrame, behaviour: str) -> pd.DataFrame:
+    df['Modifier'] = df[['Behavior']].apply(
+        lambda x: x['Behavior'].split("_", 1)[1] if len(x['Behavior'].split('_')) > 1 else '', axis=1)
+    df['Behavior'] = df[['Behavior']].apply(lambda x: x['Behavior'].split(
+        "_", 1)[0] if len(x['Behavior'].split('_')) > 1 else x['Behavior'], axis=1)
+
+    return df
+
+def get_bouts(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This function creates an interval tree,
+    so that multiple overlapping behaviours 
+    can be considered part of the same bout.
+    '''
+    df = df.sort_values(['Time'], ascending=[True])
+    df = match_start_and_stop_for_behaviour(df)
+    tree = IntervalTree(Interval(start, stop) for start, stop in zip(df['Time_start'], df['Time_stop']))
+
+    tree.merge_overlaps()
+    intervals_df = pd.DataFrame([(interval.begin, interval.end, i+1) for i, interval in enumerate(tree)], columns=['Time_start', 'Time_stop', 'Interval_id'])
+    df['Interval_id'] = np.nan
+
+    for i, row in df.iterrows():
+        for j, interval_row in intervals_df.iterrows():
+            if interval_row['Time_start'] <= row['Time_start'] <= interval_row['Time_stop']:
+                df.at[i, 'Interval_id'] = interval_row['Interval_id']
+                break
+
+    return df
+
+def get_behaviour_data_for_each_subject(df: pd.DataFrame) -> pd.DataFrame:
+    # TODO: figure out how to present this 
+    pass
+
+def get_column_names_for_summary_table(name: str) -> str:
+    pass
+
+def get_time_doing_behaviour(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def get_total_stereotyping_duration(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def write_to_excel(df: pd.DataFrame) -> None:
+    pass
+
+
+def match_start_and_stop_for_behaviour(df: pd.DataFrame) -> pd.DataFrame:
+    start_dataframe = df[df['Behavior type'] == 'START']
+    stop_dataframe = df[df['Behavior type'] == 'STOP']
+
+    start_dataframe['start_id'] = start_dataframe.groupby(
+        ['Subject', 'Behavior', 'Observation id', 'Observation date',
+         'Observation duration']).cumcount()
+    stop_dataframe['stop_id'] = stop_dataframe.groupby(
+       ['Subject', 'Behavior', 'Observation id', 'Observation date',
+         'Observation duration']).cumcount()
+
+    merged_df = pd.merge(start_dataframe, stop_dataframe, how='left', left_on=[
+        'Subject', 'Behavior', 'Observation id', 'start_id', 'Modifier', 'Observation date', 'Observation duration'],
+        right_on=[
+            'Subject', 'Behavior', 'Observation id', 'stop_id', 'Modifier', 'Observation date', 'Observation duration'], 
+            suffixes=["_start", "_stop"])
+
+    merged_df.drop(columns=['start_id', 'stop_id',
+                   'Behavior type_start', 'Behavior type_stop'], inplace=True)
+
+    merged_df['Behaviour Duration (s)'] = merged_df[['Time_stop', 'Time_start']].apply(
+        lambda x: x['Time_stop'] - x['Time_start'], axis=1)
+
+    return merged_df 
+
+
+def import_input_files(data_files) ->  dict[str, pd.DataFrame]:
+    input_data_tables_dict= {}
+    columns_of_interest = ['Observation id',
+                           'Subject', 'Behavior',
+                           'Behavior type', 'Time', 'Observation date', 'Observation duration']
+    if data_files is not None:
+        for file in data_files:
+            file.seek(0)
+            print(f'Reading columns for file {file.name}')
+            input_data_tables_dict[file.name] = pd.read_csv(
+                file, delimiter='\t')
+            input_data_tables_dict[file.name].drop(columns=[col for col in input_data_tables_dict[file.name]
+                                                            if col not in columns_of_interest], inplace=True)
+
+    return input_data_tables_dict
+
+
+def run_pipeline(subject, df):
+    data_by_subject = get_behaviour_modifiers(df, 'Behavior')
+    data_by_subject = get_bouts(data_by_subject)
+    return data_by_subject
