5fed3011faf6c2fa855d1924bab4f3b910db0d35
Nancy Cantu <cantu.nancy@gmail.com>
2024-04-17 07:00:02 -0400

    remove per-subject filter
    this is a slight regression but a necessary step
    
    also means the backend must be rewritten

--- app.py
+++ app.py
@@ -5,15 +5,13 @@ import pandas as pd
 
 titles = ['Data', 'Behaviour Stats', 'Bouts', 'Bout Stats', 'Proportion of time doing each behaviour in each area']
 
-def run_and_concatenate(subjects, dfs):
+def run_and_concatenate(dfs):
     all_results = [[], [], [], []]
-    for subject, data in subjects.items():
-        results = be.run_pipeline(subject, data)
+    for file, data in dfs.items():
+        results = be.run_pipeline(file, data)
         for i in range(4):
             all_results[i].append(results[i])
-
-    concat_data = [pd.concat(results) for results in all_results]
-    return concat_data
+    return [pd.concat(results) for results in all_results]
 
 def main():
     st.set_page_config(page_title="Behavioural analysis pipeline", page_icon="🧠", initial_sidebar_state="auto", 
@@ -24,28 +22,15 @@ def main():
     st.write("This pipeline takes in .csv or .tsv files and outputs a csv file with behavioural analysis data and statistics.")
     data_files = st.file_uploader("Upload your data files", type=['csv', 'tsv'], accept_multiple_files=True)
     dfs = be.import_input_files(data_files)
+    
     if dfs:
-        all_subjects = {}
-        for file, data in dfs.items():
-            data_by_subject = be.separate_data_by_subject(data)
-            all_subjects.update(data_by_subject)
-
-        subject_list = list(all_subjects.keys())
-        subject_list.insert(0, 'All Subjects')
-        selected_subject = st.selectbox('Select a subject to run the pipeline for', subject_list)
-
-        if selected_subject in all_subjects:
-            subjects = {selected_subject: all_subjects[selected_subject] for file, data in dfs.items()}
-            all_data = run_and_concatenate(subjects, dfs)
-        elif selected_subject == 'All Subjects':
-            all_data = run_and_concatenate(all_subjects, dfs)
+        all_data = run_and_concatenate(dfs)
 
         for title, data in zip(titles, all_data):
             data.fillna(0, inplace=True)
             data.sort_index(axis=1, inplace=True)
             st.subheader(title)
             st.dataframe(data)
-
 
 if __name__ == "__main__":
     main()
